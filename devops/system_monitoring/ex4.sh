5cb5df7c45cb65f30f74755e58da408b16575de5
Gal Elhiani <galelhiani@gmail.com>
2026-06-25 05:07:12 +0000

    Edit ex4.sh

--- devops/system_monitoring/ex4.sh
+++ devops/system_monitoring/ex4.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#this script generates a random number from 1-10 and succeeds if the number is greater than 10
+#this script generates a random number from 1-10 and succeeds if the number is greater than 5
 #Tested by: Amir malul
 #Authored by: Gal Elhiani
 
@@ -10,6 +10,7 @@ if [ "$num" -gt 5 ]; then
 
 	echo "success!"
 	echo "$num"
+	logger "Code ran successfully"
 else
 	echo "failure!"
 	echo "$num"
