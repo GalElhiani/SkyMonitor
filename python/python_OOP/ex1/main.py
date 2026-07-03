efbec461f5041af83ea813131a1f2b9d816b32f0
gal.elhiani <galelhiani@gmail.com>
2026-07-03 15:59:47 +0000

    testing .gitignore

--- python/python_OOP/ex1/main.py
+++ python/python_OOP/ex1/main.py
@@ -6,6 +6,8 @@ import point
 
 p1 = point.Point(3, 4)
 p2 = point.Point(14, 20)
+p3 = point.Point(10, 100)
 
 print(p1.distance_from_origin())
 print(p2.distance_from_origin())
+print(p3.distance_from_origin())
